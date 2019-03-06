c02b7ca8a1765b6f325712395e031a5d437cdfb8
renraleerning <renraleerning@.github.com>
2019-03-06 09:11:08 +0700

    no4

---
+++ src/SenjataKilat.java
@@ -0,0 +1,9 @@
+/*
+    Contoh implementasi kelas Senjata
+ */
+public class SenjataKilat extends  Senjata {
+    //constructor
+    public SenjataKilat() {
+        kekuatan = 40; nama ="Senjata_Kilat";
+    }
+}
